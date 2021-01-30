591ab740dcc3c1dd0825f9bbc29106aa83520434
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2021-01-30 02:10:32 -0300

    test material ui

---
+++ components/test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function haha() {
+
+    return (
+        <div>
+            Hello
+        </div>)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { signIn, signOut, useSession } from 'next-auth/client'
 
+import { Button } from '@material-ui/core'
+
 export default function Page() {
   const [ session, loading ] = useSession()
 
   return <>
     {!session && <>
       Not signed in <br/>
-      <button onClick={signIn}>Sign in</button>
+      <Button variant='outlined' color='primary' onClick={signIn}>Sign in</Button>
     </>}
+    
     {session && <>
       Signed in as {session.user.email} <br/>
-      <button onClick={signOut}>Sign out</button>
+      <Button variant='outlined' color='secondary' onClick={signOut}>Sign out</Button>
     </>}
   </>
 }
